a8a528815dae9f07bf7a72f78bfe5d9460ca1430
Tom Kay <oridan82@gmail.com>
2020-06-24 13:25:54 +0100

    flag to specify the path of config file (#1)
    * flag to specify the path of config file
    
    * remove unnecessary nesting
    
    * condense

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	cli "gopkg.in/alecthomas/kingpin.v2"
 	"log"
 	"net/http"
 	"os"
@@ -8,20 +9,52 @@ import (
 	"path/filepath"
 )
 
+var (
+	configPath = cli.Flag("config", "Path to the config yaml").Short('c').String()
+)
+
 // For testing purposes set this to false to disable the JWT check
 const EnableAuthCheck = true
 
 func main() {
-	configFile := "config.yaml"
+	cli.Parse()
 
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err == nil {
-		configFile = path.Join(dir, configFile)
+	var configPaths []string
+	if *configPath != "" {
+		// config file specified
+		if filepath.IsAbs(*configPath) {
+			// absolute path
+			configPaths = append(configPaths, *configPath)
+		} else if cwd, err := os.Getwd(); err == nil {
+			// relative path
+			configPaths = append(configPaths, path.Join(cwd, *configPath))
+		}
+	} else {
+		if cwd, err := os.Getwd(); err == nil {
+			// no config specified, search in current directory
+			configPaths = append(configPaths, path.Join(cwd, "config.yaml"))
+		}
+
+		if binPath, err := filepath.Abs(filepath.Dir(os.Args[0])); err == nil {
+			// search in binary directory
+			configPaths = append(configPaths, path.Join(binPath, "config.yaml"))
+		}
 	}
 
-	cfg, err := LoadConfig(configFile)
-	if err != nil {
-		log.Fatal(err.Error())
+	var cfg *Config
+	for _, configFile := range configPaths {
+		info, err := os.Stat(configFile)
+		if !os.IsNotExist(err) && !info.IsDir() {
+			cfg, err = LoadConfig(configFile)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+			break
+		}
+	}
+
+	if cfg == nil {
+		log.Fatal("Config file not found")
 	}
 
 	p := NewProxy(cfg)
