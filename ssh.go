4bdbbba8fd77fb213682417b3a7f3e2996e7d2e9
Tom Kay <oridan82@gmail.com>
2023-02-01 11:04:28 +0000

    reuse local connection if possible

--- ssh.go
+++ ssh.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"os"
 	"strings"
@@ -25,39 +24,44 @@ func startSshTunnel(c *Config) {
 	tunnel, publishPort := strings.Join(tunnelSplit[:len(tunnelSplit)-1], ":"), tunnelSplit[len(tunnelSplit)-1]
 
 	// ssh-agent(1) provides a UNIX socket at $SSH_AUTH_SOCK.
-	socket := os.Getenv("SSH_AUTH_SOCK")
-	conn, err := net.Dial("unix", socket)
-	if err != nil {
-		log.Fatalf("Failed to open SSH_AUTH_SOCK: %v", err)
-	}
-
-	agentClient := agent.NewClient(conn)
+	agentConnection, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	logs.FatalIf(err, "opening SSH_AUTH_SOCK")
 
 	sshConfig := &ssh.ClientConfig{
 		User:            username,
-		Auth:            []ssh.AuthMethod{ssh.PublicKeysCallback(agentClient.Signers)},
+		Auth:            []ssh.AuthMethod{ssh.PublicKeysCallback(agent.NewClient(agentConnection).Signers)},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
-	sshc, err := ssh.Dial("tcp", tunnel, sshConfig)
+	sshClient, err := ssh.Dial("tcp", tunnel, sshConfig)
 	logs.FatalIf(err, "dialing ssh server")
-	defer func() { _ = sshc.Close() }()
+	defer func() { _ = sshClient.Close() }()
 
 	// Listen on remote server port
-	listener, err := sshc.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", publishPort))
+	listener, err := sshClient.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", publishPort))
 	logs.FatalIf(err, "opening port on remote server")
 	defer func() { _ = listener.Close() }()
 
 	logs.Info("listening on remote server", zap.String("host", listener.Addr().String()))
 
+	var local net.Conn
+	localConnected := false
 	for {
 		remote, err := listener.Accept()
 		logs.FatalIf(err, "error accepting connection")
 
-		local, err := net.Dial("tcp", c.ListenAddress)
-		logs.FatalIf(err, "dialing local service")
+		if localConnected == false {
+			local, err = net.Dial("tcp", c.ListenAddress)
+			logs.FatalIf(err, "dialing local service")
+			localConnected = true
+		}
 
-		handleClient(local, remote)
+		if err = handleClient(local, remote); err != nil {
+			_ = local.Close()
+			localConnected = false
+			logs.ErrorIf(err, "dialing local service")
+		}
+
 		_ = remote.Close()
 	}
 }
@@ -65,22 +69,22 @@ func startSshTunnel(c *Config) {
 // From https://sosedoff.com/2015/05/25/ssh-port-forwarding-with-go.html
 // Handle local client connections and tunnel data to the remote server
 // Will use io.Copy - http://golang.org/pkg/io/#Copy
-func handleClient(local net.Conn, remote net.Conn) {
-	chDone := make(chan bool)
+func handleClient(local net.Conn, remote net.Conn) error {
+	chDone := make(chan error)
 
 	// Start remote -> local data transfer
 	go func() {
 		_, err := io.Copy(local, remote)
-		logs.FatalIf(err, "error while copy remote->local")
-		chDone <- true
+		logs.ErrorIf(err, "error while copy remote->local")
+		chDone <- err
 	}()
 
 	// Start local -> remote data transfer
 	go func() {
 		_, err := io.Copy(remote, local)
-		logs.FatalIf(err, "error while copy local->remote")
-		chDone <- true
+		logs.ErrorIf(err, "error while copy local->remote")
+		chDone <- err
 	}()
 
-	<-chDone
+	return <-chDone
 }
